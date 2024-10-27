bfca47324a3a0c8199aaf2240dde50666408e7b1
Diego <a22diegosg@iessanclemente.net>
2024-10-27 20:06:00 +0100

    melloras formulario engadir e rexistrar/logear

--- boxGal/forms.py
+++ boxGal/forms.py
@@ -7,17 +7,43 @@ class CompetidorForm(forms.ModelForm):
     class Meta:
         model = Competidor
         fields = ['nome_competidor', 'apelidos_competidor', 'foto_competidor', 'peso_competidor', 'vitorias', 'derrotas', 'empates']
+        widgets = {
+            'nome_competidor': forms.TextInput(attrs={'class': 'form-control'}),
+            'apelidos_competidor': forms.TextInput(attrs={'class': 'form-control'}),
+            'foto_competidor': forms.ClearableFileInput(attrs={'class': 'form-control'}),
+            'peso_competidor': forms.NumberInput(attrs={'class': 'form-control'}),
+            'vitorias': forms.NumberInput(attrs={'class': 'form-control'}),
+            'derrotas': forms.NumberInput(attrs={'class': 'form-control'}),
+            'empates': forms.NumberInput(attrs={'class': 'form-control'}),
+        }
         
 class CombateForm(forms.ModelForm):
     class Meta:
         model = Combate
         fields = ['boxeador_azul', 'boxeador_vermello', 'evento_combate', 'peso_combate', 'categoria_combate', 'arbitro', 'resultado', 'data_combate']
+        widgets = {
+            'boxeador_azul': forms.Select(attrs={'class': 'form-select'}),
+            'boxeador_vermello': forms.Select(attrs={'class': 'form-select'}),
+            'evento_combate': forms.Select(attrs={'class': 'form-select'}),
+            'peso_combate': forms.TextInput(attrs={'class': 'form-control'}),
+            'categoria_combate': forms.TextInput(attrs={'class': 'form-control'}),
+            'arbitro': forms.TextInput(attrs={'class': 'form-control'}),
+            'resultado': forms.TextInput(attrs={'class': 'form-control'}),
+            'data_combate': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+        }
         
 class EventoForm(forms.ModelForm):
     class Meta:
         model = Evento
         fields = ['nome_evento', 'lugar_evento', 'data_evento', 'foto_evento']
+        widgets = {
+            'nome_evento': forms.TextInput(attrs={'class': 'form-control'}),
+            'lugar_evento': forms.TextInput(attrs={'class': 'form-control'}),
+            'data_evento': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+            'foto_evento': forms.ClearableFileInput(attrs={'class': 'form-control'}),
+        }
         
+
 class RexistroUsuarioForm(UserCreationForm):
     class Meta:
         model = Usuario
--- proxectoBoxeo/settings.py
+++ proxectoBoxeo/settings.py
@@ -121,7 +121,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+
 STATIC_URL = 'static/' # Necesario co proxecto en local para ver as imaxes
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'boxGal/static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
